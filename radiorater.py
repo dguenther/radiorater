c637a301c82a9101be4e6f96688b6358aa7e0c0b
Derek Guenther <dguenther9@gmail.com>
2011-08-23 01:04:04 -0500

    Altered scraper to work with new layout

--- radiorater.py
+++ radiorater.py
@@ -1,7 +1,7 @@
 ## 
 ## animenfo rating bot
 ##
-## Last Updated: 05-14-2011
+## Last Updated: 08-03-2011
 ## Version 2
 ##
 ## future improvements:
@@ -61,7 +61,7 @@ br.addheaders = [('User-agent',USER_AGENT)]
 #br.set_debug_responses(True)
 
 def safeOpen(url, data=None):
-    ''' Retry opening URL until it is successful'''
+    ''' Retries opening URL until it opens successfully '''
     success = False
     while success == False:
         try:
@@ -71,11 +71,12 @@ def safeOpen(url, data=None):
                 br.open(url,data)
             success = True
         except:
-            # if url opening fails, sleep for 3 seconds
+            # if url opening fails, sleep for 2 seconds
             success = False
-            time.sleep(3)
+            time.sleep(2)
 
 def login():
+    ''' Logs into the site and saves a cookie to the cookiejar '''
     values = {'frompage':'index.php',
             'username':USERNAME,
             'password':PASSWORD
@@ -86,10 +87,10 @@ def login():
     cj.save()
 
 def getStdDev(rates):
-    ''' Determines a standard deviation for rating based on the number of people
+    ''' Returns a standard deviation used for rating based on the number of people
         who have already rated a song '''
-    # wouldn't mind finding a smoother way to do this, maybe some sort of
-    # exponential curve
+    # These are fairly arbitrary numbers. I wouldn't mind finding
+    # a smoother way to do this, maybe some sort of exponential curve
     if rates == 0:
         return 1.5
     elif rates < 100:
@@ -100,6 +101,8 @@ def getStdDev(rates):
         return 0.5
 
 def generateRating(currentRating, rates):
+    ''' Generates an integer rating between 1 and 10 based on the current
+        song rating and the number of times it has been rated '''
     try:
         rating = float(currentRating)
     except:
@@ -114,7 +117,8 @@ def generateRating(currentRating, rates):
         return myRating
 
 def favoriteSong():
-    if (findMechanizeLinkById('np_delfav') == None):
+    ''' Add a song to the favorites list '''
+    if (findMechanizeLinkById('np_removefav') == None):
         favoriteLink = findMechanizeLinkById('np_addfav')
         if (favoriteLink != None):
             br.follow_link(favoriteLink)
@@ -122,13 +126,14 @@ def favoriteSong():
             print "Added this song to favorites."
         else:
             print "Unable to favorite song - no favorites links detected."
-    else:
+    else:  
         print "Already favorited this song."
 
 def getContent():
     soup = BeautifulSoup(br.response().read())
     items = soup.findAll('td', limit=2)
-    return items[1]    
+    print items[1]
+    return items[1]
 
 def outputSongInfo():
     text = getContent()
@@ -138,42 +143,59 @@ def outputSongInfo():
     return data
 
 def findMechanizeLinkById(desiredId):
+    ''' Given the ID of a link on the page, returns that link as a mechanize link '''
     for link in br.links():
         attrDict = dict(link.attrs)
         try:
             if (attrDict['id'] == desiredId):
                 return link
-        # if KeyError is thrown while accessing id, link has no id, so skip
+        # if KeyError is thrown while accessing id, link has no id, so skip it
         except KeyError:
             pass
     return None
+    
+def findMechanizeLinksWithClass(desiredClass):
+    ''' Given a class, returns a list of mechanize links that have that class '''
+    linkList = []
+    for link in br.links():
+        attrDict = dict(link.attrs)
+        try:
+            if (desiredClass in attrDict['class']):
+                linkList.append(link)
+        # if KeyError is thrown while accessing class, link has no class, so skip it
+        except KeyError:
+            pass
+    return linkList
 
-def findSoupItemById(desiredId):
+def findSoupItemById(tagName, desiredId):
     soup = BeautifulSoup(br.response().read())
-    return soup.find(attrs={'id' : desiredId})
+    return soup.find(tagName, attrs={'id' : desiredId})
 
 def getSeconds():
-    span = findSoupItemById('np_timer')
-    return int(span['rel'])
+    timeSpan = findSoupItemById('span','np_timer')
+    return int(timeSpan['rel'])
 
 def songHasBeenRated():
-    rateText = findSoupItemById('rateform')
+    rateText = findSoupItemById('form', 'rateform')
     rateText = rateText.contents[4].strip()
     return (rateText.find('Change your rating for this song:') != -1)
 
 def canRate(currentTime):
+    ''' Returns true if the current time is between START_HOUR and END_HOUR '''
     date = currentTime.date()
     startDatetime = datetime.datetime.combine(date, datetime.time(START_HOUR,0,0))
     endDatetime = datetime.datetime.combine(date, datetime.time(END_HOUR,0,0))
     return (startDatetime < currentTime and endDatetime > currentTime)
 
 def getSecondsUntilTomorrow(currentTime):
+    ''' Returns the number of seconds until START_HOUR '''
     date = currentTime.date()
     date = date + datetime.timedelta(days=1)
     tomorrowDatetime = datetime.datetime.combine(date, datetime.time(START_HOUR,0,0))
     delta = tomorrowDatetime - currentTime
     return delta.seconds
 
+# main (infinite) loop
 while(True):
     currentTime = datetime.datetime.now()
     while(canRate(currentTime)):
@@ -188,10 +210,11 @@ while(True):
         seconds = getSeconds()
         if seconds <= 45:
             # if <= 30, rate and set timer to timer + ~10
-
             if not (songHasBeenRated()):
-                text = getContent()
-                rating = text.contents[12].strip().strip('Rating: ')
+                soup = BeautifulSoup(br.response().read())
+                items = soup.findAll('td', limit=3)
+                text = items[2]
+                rating = text.contents[5].strip().strip('Rating: ')
                 currentRating = rating.split('/', 1)[0]
                 rates = rating.split('rate', 1)[0]
                 rates = rates.split('(')[1]
@@ -224,8 +247,9 @@ while(True):
             seconds = seconds - random.randint(15,45)
             print 'Sleeping for %d seconds...' % (seconds)
             time.sleep(seconds)
+        currentTime = datetime.datetime.now()
 
     sleepTime = getSecondsUntilTomorrow(currentTime)
-    print 'Sleeping for %d hours...' % (sleepTime / 3600)
+    print 'Sleeping for %d hours, %d minutes...' % ((sleepTime / 3600), ((sleepTime % 3600)/ 60))
     time.sleep(sleepTime)
 
